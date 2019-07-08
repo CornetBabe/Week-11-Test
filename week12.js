af06e2110a6d0b062d077853f0af87fdd77d6d04
CornetBabe <satfieldcutts@bournemouth.ac.uk>
2019-07-08 13:36:30 +0100

    Add initial files

---
+++ week12.js
@@ -0,0 +1,59 @@
+console.log("Helloworld from javascript!");
+
+//Task 1
+console.log("Hello World!");
+console.log("I’ve made it to university");
+console.log("And this is my first programming task.");
+console.log("And this is my first \tprogramming task.");
+
+//TIMS ANSWERS
+/*
+* POP wk 12 JavaScript tasks
+*/
+var jsLoaded = "JSLOADED";
+//Ignore everything above this comment and do not delete it.
+
+
+//Write your JavaScript code in this file below this comment.
+
+
+//Write your Hello World program below:
+
+console.log("Hello World!");
+
+
+
+
+//Write your other lab tasks below:
+
+console.log("Hello World!");
+console.log("I’ve made it to university");
+console.log("And this is my first programming task.");
+
+
+console.log("Name:   Bob Smith");
+console.log("Add:    Bournemouth University");
+
+
+console.log("Name:\t\tBob Smith");
+console.log("Add:\t\tBournemouth University");
+
+
+console.log("        *");
+console.log("       ***");
+console.log("      *****");
+console.log("     *******");
+console.log("    *********");
+console.log("   ***********");
+console.log("  *************");
+console.log(" ***************");
+
+
+
+console.log("H:\\notes");
+console.log("H:\\timetable");
+console.log("C:\\programming\\week11\\HelloWorld.java");
+console.log("X:\\\\Valid\\\\\?");
+console.log("\"Programming is an art form that fights back\" Anon")
+
+
